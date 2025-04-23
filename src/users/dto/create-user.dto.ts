ed28b42e093249ad6ad3f35f7077ff1053b8bf23
Lela Fieta <lelafieta11@gmail.com>
2025-04-23 20:18:45 +0100

    Creating UserDTOS

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsOptional, IsString, Length } from 'class-validator';
+
+export enum Role {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  fullName?: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsEnum(Role)
+  role: Role;
+}
